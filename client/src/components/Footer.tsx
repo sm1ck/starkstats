9b470b7f9f95838eaeb275f31b7037818188ebe0
sm1ck <dosdosnope@gmail.com>
2023-11-04 01:18:31 +0500

    Change footer for client

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,8 +1,11 @@
+import { useTranslation } from "react-i18next";
+
 export const Footer = () => {
+    const { t } = useTranslation();
+
     return (
         <footer>
-            <a href="https://github.com/sm1ck/starkstats" target="_blank" rel="noreferrer"><img src="icons/github.svg" alt="" className="links-logo" /></a>
-            <a href="https://t.me/web3_coding" target="_blank" rel="noreferrer"><img src="icons/telegram.svg" alt="" className="links-logo" /></a>
+            <span>{t("footerForEcosystem1")} StarkNet {t("footerForEcosystem2")}, 2023.</span> <a href="https://github.com/sm1ck/starkstats" target="_blank" rel="noreferrer"><img src="icons/github.svg" alt="" className="links-logo" /></a> <a href="https://t.me/web3_coding" target="_blank" rel="noreferrer"><img src="icons/telegram.svg" alt="" className="links-logo" /></a>
         </footer>
     )
 };
